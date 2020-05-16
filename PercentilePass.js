e000445261389492f6a0048e1ef1f8c16ec7a261
James Hobin <hobinjk@gmail.com>
2020-05-16 00:31:34 -0400

    Add PercentilePass

---
+++ PercentilePass.js
@@ -0,0 +1,91 @@
+const targetDps = {};
+const allDps = {};
+
+
+function addLog(logPath, fightName, allPlayerStats) {
+  if (!targetDps[fightName]) {
+    targetDps[fightName] = {};
+  }
+  if (!allDps[fightName]) {
+    allDps[fightName] = {};
+  }
+
+  for (let player of allPlayerStats) {
+    if (!targetDps[fightName][player.spec]) {
+      targetDps[fightName][player.spec] = [];
+    }
+    if (!allDps[fightName][player.spec]) {
+      allDps[fightName][player.spec] = [];
+    }
+    targetDps[fightName][player.spec].push({
+      dps: player.targetDps,
+      logPath,
+    });
+    allDps[fightName][player.spec].push({
+      dps: player.allDps,
+      logPath,
+    });
+  }
+}
+
+let finalized = false;
+function finalize() {
+  finalized = true;
+  let summary = {};
+  for (let fightName in targetDps) {
+    summary[fightName] = {};
+    for (let spec in targetDps[fightName]) {
+      targetDps[fightName][spec].sort((a, b) => a.dps - b.dps);
+      allDps[fightName][spec].sort((a, b) => a.dps - b.dps);
+      let ds = targetDps[fightName][spec];
+      summary[fightName][spec] = {
+        min: ds[0],
+        max: ds[ds.length - 1],
+      };
+    }
+  }
+  console.log('fun info', summary);
+  console.log('funner info',
+              summary['Gorseval the Multifarious']);
+}
+
+function binarySearch(list, val) {
+  // Thanks Wikipedia
+  let l = 0;
+  let r = list.length - 1;
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2);
+    if (list[m].dps < val) {
+      l = m + 1;
+    } else if (list[m].dps > val) {
+      r = m - 1;
+    } else {
+      return m;
+    }
+  }
+  return Math.floor((l + r) / 2);
+}
+
+function percentile(list, val) {
+  let i = binarySearch(list, val);
+  return i / list.length * 100;
+}
+
+function getPercentiles(log, player) {
+  if (!finalized) {
+    finalize();
+  }
+
+  const tds = targetDps[log.fightName][player.spec];
+  const ads = targetDps[log.fightName][player.spec];
+  return {
+    targetDps: percentile(tds, player.targetDps),
+    allDps: percentile(ads, player.targetDps),
+  };
+}
+
+module.exports = {
+  addLog,
+  getPercentiles,
+  finalize,
+};
